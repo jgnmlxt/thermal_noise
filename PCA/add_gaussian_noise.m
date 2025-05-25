3bf62f7e0d9e6ac4a1f9adb74e5cd8d366351352
jgnmlxt <jgnmlxt@users.noreply.github.com>
2025-05-25 08:58:52 -0400

    comparing different definitions of X

--- PCA/add_gaussian_noise.m
+++ PCA/add_gaussian_noise.m
@@ -1,11 +1,11 @@
-function GT_noise=add_gaussian_noise(GT,variance,repetition)
+function GT_noise=add_gaussian_noise(GT,sigma,repetition)
     
     [rows,cols]=size(GT);
     GT_noise=zeros(repetition,rows,cols);
 
     for i=1:repetition
-        noise_real=variance*randn(size(GT));
-        noise_complex=variance*randn(size(GT));
+        noise_real=sigma*randn(size(GT));
+        noise_complex=sigma*randn(size(GT));
     
         GT_noise(i,:,:)=GT+noise_real+1j*noise_complex;
     end
